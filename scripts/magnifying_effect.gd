6e11d4787d664c18403dc547bca9a1a618459de1
SwordAndTea <xiangweiqaz@gmail.com>
2025-09-27 23:00:26 -0400

    fix input error on magnifying effect

--- scripts/magnifying_effect.gd
+++ scripts/magnifying_effect.gd
@@ -3,15 +3,18 @@ extends Sprite2D
 var magnification := 2.0
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("focus") and Global.is_magnifier_pick_up:
-		visible = true
-		position = get_global_mouse_position()
-		z_index = 100
+	if event.is_action_pressed("focus"):
+		if !visible and Global.is_magnifier_pick_up:
+			visible = true
+			magnification = 2.0
+			position = get_global_mouse_position()
+		elif visible == true:
+			visible = false
 		
 	elif event is InputEventMouseMotion:
 		position = get_global_mouse_position()
 		
-	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) and visible:
 		if magnification == 2.0:
 			magnification = 4.0
 			material.set_shader_parameter("magnification", 4.0)
